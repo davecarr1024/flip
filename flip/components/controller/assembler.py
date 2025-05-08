204dcb3ca56a5c37bc906e73d96f5da9f67a6982
davecarr1024 <davecarr1024@gmail.com>
2025-05-08 19:10:10 +0000

    cleanup

--- flip/components/controller/assembler.py
+++ flip/components/controller/assembler.py
@@ -44,15 +44,3 @@ class Assembler(Errorable):
                             )
                             data[address] = self.__format.encode_controls(step)
         return InstructionMemory(data=data, format=self.__format)
-        # data = dict[int, int]()
-        # for instruction in self.__instruction_set:
-        #     for statuses in self._expand_statuses(dict(instruction.statuses)):
-        #         for step_index, step in enumerate(instruction):
-        #             data[
-        #                 self.__format.encode_address(
-        #                     instruction.opcode,
-        #                     statuses,
-        #                     Byte(step_index),
-        #                 )
-        #             ] = self.__format.encode_controls(step.controls)
-        # return InstructionMemory(data=data, format=self.__format)
